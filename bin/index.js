b6e61e9b0b539b49fde05e86b1b5429dc56cf854
淘杰 <taojie.hjp@taobao.com>
2014-06-18 01:11:21 +0800

    update

---
+++ bin/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+var path = require("path"),
+	parser = require('../lib/kissy2cmd');
+
+var basePath = process.cwd(),
+	sourcePath = basePath,
+	buildPath;
+
+var program = require("commander"),
+	pkg = require("../package");
+
+program
+    .version(pkg.version)
+    .option('-b, --build [value]', 'build path')
+    .option('-s, --source [value]', 'source path')
+    .option('-m, --minify', 'minify or not')
+    .option('-o, --stdout', 'disbale stdout or not')
+    .option('-t, --type [value]','code style type, cmd or kissy')
+    .parse(process.argv);
+
+var options = {};
+
+if(!program.build || typeof program.build !== "string") {
+	buildPath = path.join(basePath, "../build");
+}else{
+	buildPath = path.join(basePath,program.build);
+}
+
+if(program.source && typeof program.source == "string") {
+	sourcePath = path.join(basePath,program.source);
+}
+
+parser.build({
+	src: sourcePath,
+	dest: buildPath,
+	stdout: !program.stdout,
+	minify: program.minify,
+	style:program.type
+});
